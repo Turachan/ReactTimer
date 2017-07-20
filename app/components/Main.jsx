b51f7399d28be709ac3cb535bc6171bfc8fdebbd
Appledore <turac.gurer@mebitech.com>
2017-07-20 10:33:53 +0300

    React Timer initiated.

---
+++ app/components/Main.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+
+var Main = (props) => {
+  return (
+  <div>
+    <div>
+      <div>
+        <p>Main.jsx Rendered</p>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+module.exports = Main;
+
+//STATE olmadığı için => kullanılabilir
+// var Main = React.createClass({
+//   render: function(){
+//
+// });
